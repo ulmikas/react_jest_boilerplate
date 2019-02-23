f84978eac4fe71e7fdd4b4f0177957efe053726b
nikolay.buzdalov <buzdalov@restream.rt.ru>
2019-02-23 14:07:07 +0400

    snapshot

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { mount, shallow, render } from 'enzyme';
+import { mount } from 'enzyme';
 import App from '../src/components/App';
 
 describe('<App />', () => {
   it('should render correctly tab 3', () => {
     const tree = mount(<App />);
     const tabLast = tree.find('[data-tabs="tab"]').last();
+    expect(tree.render()).toMatchSnapshot();
     tabLast.simulate('click');
-    const tabContentLast = tree.find('[data-tabs="tab-panel"]').last();
-    expect(tabContentLast).toMatchSnapshot();
+    expect(tree.render()).toMatchSnapshot();
   });
 });
