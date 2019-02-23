c9237092a5608c82c3ad3d07cc0687247fa87529
nikolay.buzdalov <buzdalov@restream.rt.ru>
2019-02-23 14:45:50 +0400

    Добавление и удаление табов

--- src/components/TabsComponent/TabsComponent.jsx
+++ src/components/TabsComponent/TabsComponent.jsx
@@ -1,29 +1,73 @@
-import {
-  Tab, Tabs, TabList, TabPanel,
-} from 'react-tabs';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import uuid from 'uuid';
+import debug from 'debug';
 
 import React, { Component } from 'react';
 
+const TABS = [
+  {
+    name: 'Title 1',
+    content: 'Content 1',
+    id: uuid(),
+  },
+  {
+    name: 'Title 2',
+    content: 'Content 2',
+    id: uuid(),
+  },
+];
+
 class TabsComponent extends Component {
+  state = {
+    tabs: TABS,
+  };
+
+  handleCloseTab = (event, id) => {
+    const { tabs } = this.state;
+
+    debug('tabs ', tabs);
+
+    this.setState({
+      tabs: tabs.filter(tab => tab.id !== id),
+    });
+  };
+
+  handleAddTab = () => {
+    const { tabs } =  this.state;
+
+    this.setState({
+      tabs: [
+        ...tabs,
+        { name: `Title ${tabs.length + 1}`, content: `Content ${tabs.length + 1}`, id: uuid() },
+      ],
+    });
+  };
+
+  renderTabName = tab => (
+    <Tab key={tab.id}>
+      <span data-tabs="tab">{tab.name}</span>
+      &nbsp;
+      <span onClick={event => this.handleCloseTab(event, tab.id)}>&times;</span>
+    </Tab>
+  );
+
+  renderTabsContent = ({id, content}) => (
+    <TabPanel key={id}>
+      <div data-tabs="tab-panel">{content}</div>
+    </TabPanel>
+  );
+
   render() {
+    const { tabs } = this.state
     return (
       <Tabs data-tabs="tabs">
-        <TabList data-tabs="tab-list">
-          <Tab><span data-tabs="tab">Title 1</span></Tab>
-          <Tab><span data-tabs="tab">Title 2</span></Tab>
-        </TabList>
+      <TabList>
+        {tabs.map(this.renderTabName)}
+        <span onClick={this.handleAddTab}>Add</span>
+      </TabList>
 
-        <TabPanel>
-          <div data-tabs="tab-panel">
-            <h2>Any content 1</h2>
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div data-tabs="tab-panel">
-            <h2>Any content 2</h2>
-          </div>
-        </TabPanel>
+        {tabs.map(this.renderTabsContent)}
       </Tabs>
     );
   }
