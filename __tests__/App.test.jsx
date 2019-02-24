c1eeea6039f38e19ddf7ec822b44a6f8eda7fa59
nikolay.buzdalov <buzdalov@restream.rt.ru>
2019-02-24 11:30:37 +0400

    page object

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,40 +1,66 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import Coockies from 'js-cookie';
 import App from '../src/components/App';
+
+const TABS_SELECTOR = 'ul[data-test="tabs-list"]';
+const TAB_SELECTOR = 'li[data-test="tab"]';
+const REMOVE_TAB_SELECTOR = '[data-test="remove-tab"]';
+const ADD_TAB_SELECTOR = '[data-test="add-tab"]';
+const SELECTED_TAB = '[aria-selected="true"]';
+const UNSELECTED_TAB = '[aria-selected="false"]';
+
+const treeObject = tree => ({
+  getTabsContainer: () => tree.find(TABS_SELECTOR),
+  getTabsList: () => tree.find(TAB_SELECTOR),
+  getLastAnchor: () => tree.find(TAB_SELECTOR).last(),
+  getNthTab: n => tree.find(TAB_SELECTOR).at(n),
+  getRemoveButton: tab => tab.find(REMOVE_TAB_SELECTOR),
+  getAddButton: () => tree.find(ADD_TAB_SELECTOR),
+  reloadApp: () => tree.unmount().mount(),
+});
 
 describe('<App />', () => {
   it('adds tab', () => {
     const tree = mount(<App />);
-    const addTabButton = tree.find('[data-test="add-tab"]');
+    const tObj = treeObject(tree);
+    const addTabButton = tObj.getAddButton();
 
     addTabButton.simulate('click');
-    expect(tree).toContainMatchingElements(6, 'li[data-test="tab"]');
+    expect(tree).toContainMatchingElements(6, TAB_SELECTOR);
   });
 
   it('remove tab', () => {
     const tree = mount(<App />);
-    const tabListBefore = tree.find('ul[data-test="tabs-list"]');
-    const tabs = tree.find('li[data-test="tab"]');
-    const tabToRemove = tabs.at(1);
-    const removeTab = tabToRemove.find('[data-test="remove-tab"]');
+    const tObj = treeObject(tree);
+    const tabListBefore = tObj.getTabsContainer();
+    const tabToRemove = tObj.getNthTab(1);
+    const removeTab = tObj.getRemoveButton(tabToRemove);
 
-    expect(tabListBefore).toContainMatchingElements(5, 'li[data-test="tab"]');
+    expect(tabListBefore).toContainMatchingElements(5, TAB_SELECTOR);
 
     removeTab.simulate('click');
-    const tabListAfter = tree.find('ul[data-test="tabs-list"]');
+    const tabListAfter = tObj.getTabsContainer();
 
-    expect(tabListAfter).toContainMatchingElements(4, 'li[data-test="tab"]');
+    expect(tabListAfter).toContainMatchingElements(4, TAB_SELECTOR);
   });
 
   it('set active tab', () => {
     const tree = mount(<App />);
-    const tabs = tree.find('li[data-test="tab"]');
-    const tabToBeActive = tabs.at(1);
+    const tObj = treeObject(tree);
+    const tabToBeActive = tObj.getNthTab(1);
 
     tabToBeActive.simulate('click');
-    const tabsAfter = tree.find('li[data-test="tab"]');
+    // const tabsAfter = tObj.getTabsList();
 
-    expect(tabsAfter.at(0)).toMatchSelector('[aria-selected="false"]');
-    expect(tabsAfter.at(1)).toMatchSelector('[aria-selected="true"]');
+    expect(tObj.getNthTab(0)).toMatchSelector(UNSELECTED_TAB);
+    expect(tObj.getNthTab(1)).toMatchSelector(SELECTED_TAB);
+  });
+
+  it('check that active tab sets from coockies', () => {
+    const tree = mount(<App />);
+    const tObj = treeObject(tree);
+    const getIndexFromCoockies = jest.fn(() => Coockies.get('activeTab'));
+    expect(tObj.getNthTab(getIndexFromCoockies())).toMatchSelector(SELECTED_TAB);
   });
 });
